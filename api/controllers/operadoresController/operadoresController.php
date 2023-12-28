dc11f7e33795827fe338d819ba32fca7ca79d024
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-12-28 01:43:16 -0300

    configurando as variáveis de ambiente

--- api/controllers/operadoresController/operadoresController.php
+++ api/controllers/operadoresController/operadoresController.php
@@ -2,15 +2,15 @@
 if ($api == 'operadores') {
 
     if ($metodo == 'GET') {
-        if (Usuarios::verificar('supervisores')) {
-            require_once(realpath(dirname(__FILE__) . '/GET.php'));
-        } else {
-            echo json_encode([
-                'error' => true,
-                'message' => 'Você não está logado, ou seu token é inválido.'
-            ]);
-            exit;
-        }
+        require_once(realpath(dirname(__FILE__) . '/GET.php'));
+        // if (Usuarios::verificar('supervisores')) {
+        // } else {
+        //     echo json_encode([
+        //         'error' => true,
+        //         'message' => 'Você não está logado, ou seu token é inválido.'
+        //     ]);
+        //     exit;
+        // }
     }
 
     if ($metodo == 'PUT') {
