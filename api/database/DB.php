7f8eb88d3ef60d967c0bc1ca53f296fdd5c2930d
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-12-28 02:18:05 -0300

    ajuste na conexao com o banco

--- api/database/DB.php
+++ api/database/DB.php
@@ -4,21 +4,16 @@ class DB
     public static function connect()
     {
 
-        // $host = getenv('DB_HOST');
-        // $base = getenv('DB_BASE');
-        // $user = getenv('DB_USER');
-        // $pass = getenv('DB_PASS');
-        // $sslmode = getenv('DB_SSLMODE');
-        // $options = getenv('DB_OPTIONS');
-        $host = 'localhost';
-        $port = '5432';
-        $base = 'EscalasTrocaTurno';
-        $user = 'admin';
-        $pass = 'admin';
+        $host = getenv('DB_HOST');
+        $base = getenv('DB_BASE');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+        $sslmode = getenv('DB_SSLMODE');
+        $options = getenv('DB_OPTIONS');
 
 
 
-        // return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass;sslmode=$sslmode;options=$options");
-        return new PDO("pgsql:host=$host;port=$port;dbname=$base;user=$user;password=$pass");
+
+        return new PDO("pgsql:host=$host;dbname=$base;user=$user;password=$pass;sslmode=$sslmode;options=$options");
     }
 }
