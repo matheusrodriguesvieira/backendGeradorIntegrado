3b2323845157b33ef1af7f58dec6219e40919c49
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-12-30 20:24:05 -0300

    ajuste na estrutura e mensagem de retorno para as rotas de operadores

--- api/controllers/operadoresController/operadoresController.php
+++ api/controllers/operadoresController/operadoresController.php
@@ -2,47 +2,64 @@
 if ($api == 'operadores') {
 
     if ($metodo == 'GET') {
+
         if (!empty($_GET['login'])) {
-            $login = $_GET['login'];
-            $funcao = 'supervisores';
-            if (Usuarios::validarToken($login) && Usuarios::autorizar($funcao, $login)) {
-                require_once(realpath(dirname(__FILE__) . '/GET.php'));
-            } else {
-                echo json_encode([
-                    'error' => true,
-                    'message' => 'Você não está logado, ou seu token é inválido.'
-                ]);
-                exit;
-            }
-        } else {
             echo json_encode([
                 'error' => true,
                 'message' => "Parâmetro 'login' está ausente!"
             ]);
             exit;
         }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+        }
+
+
+        require_once(realpath(dirname(__FILE__) . '/GET.php'));
     }
 
     if ($metodo == 'PUT') {
-
         if (!empty($_GET['login'])) {
-            $login = $_GET['login'];
-            $funcao = 'supervisores';
-            if (Usuarios::validarToken($login)) {
-                require_once(realpath(dirname(__FILE__) . '/PUT.php'));
-            } else {
-                echo json_encode([
-                    'error' => true,
-                    'message' => 'Você não está logado, ou seu token é inválido.'
-                ]);
-                exit;
-            }
-        } else {
             echo json_encode([
                 'error' => true,
                 'message' => "Parâmetro 'login' está ausente!"
             ]);
             exit;
         }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+        }
+
+        require_once(realpath(dirname(__FILE__) . '/PUT.php'));
     }
 }
