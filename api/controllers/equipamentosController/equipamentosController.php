48c0fe631d75a43056f9b8f2c883aa16bdcd14da
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-12-30 20:50:30 -0300

    adição de validação para os controllers equipamentos e lista de escalas

--- api/controllers/equipamentosController/equipamentosController.php
+++ api/controllers/equipamentosController/equipamentosController.php
@@ -2,26 +2,63 @@
 if ($api == 'equipamentos') {
 
     if ($metodo == 'GET') {
+        if (empty($_GET['login'])) {
+            echo json_encode([
+                'error' => true,
+                'message' => "Parâmetro 'login' está ausente!"
+            ]);
+            exit;
+        }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+            exit;
+        }
+
         require_once(realpath(dirname(__FILE__) . '/GET.php'));
-        // if (Usuarios::verificar('supervisores')) {
-        // } else {
-        //     echo json_encode([
-        //         'error' => true,
-        //         'message' => 'Você não está logado, ou seu token é inválido.'
-        //     ]);
-        //     exit;
-        // }
     }
 
     if ($metodo == 'PUT') {
+        if (empty($_GET['login'])) {
+            echo json_encode([
+                'error' => true,
+                'message' => "Parâmetro 'login' está ausente!"
+            ]);
+            exit;
+        }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+            exit;
+        }
         require_once(realpath(dirname(__FILE__) . '/PUT.php'));
-        // if (Usuarios::verificar('supervisores')) {
-        // } else {
-        //     echo json_encode([
-        //         'error' => true,
-        //         'message' => 'Você não está logado, ou seu token é inválido.'
-        //     ]);
-        //     exit;
-        // }
     }
 }
--- api/controllers/listaEscalasController/listaEscalasController.php
+++ api/controllers/listaEscalasController/listaEscalasController.php
@@ -3,50 +3,118 @@ if ($api == 'listaEscalas') {
 
 
     if ($metodo == 'GET') {
+        if (empty($_GET['login'])) {
+            echo json_encode([
+                'error' => true,
+                'message' => "Parâmetro 'login' está ausente!"
+            ]);
+            exit;
+        }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
         require_once(realpath(dirname(__FILE__) . '/GET.php'));
-        // if ((Usuarios::verificar('supervisores')) || (Usuarios::verificar('operadores'))) {
-        // } else {
-        //     echo json_encode([
-        //         'error' => true,
-        //         'message' => 'Você não está logado, ou seu token é inválido.'
-        //     ]);
-        //     exit;
-        // }
     }
 
     if ($metodo == 'PUT') {
+        if (empty($_GET['login'])) {
+            echo json_encode([
+                'error' => true,
+                'message' => "Parâmetro 'login' está ausente!"
+            ]);
+            exit;
+        }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+            exit;
+        }
+
         require_once(realpath(dirname(__FILE__) . '/PUT.php'));
-        // if (Usuarios::verificar('supervisores')) {
-        // } else {
-        //     echo json_encode([
-        //         'error' => true,
-        //         'message' => 'Você não está logado, ou seu token é inválido.'
-        //     ]);
-        //     exit;
-        // }
     }
 
     if ($metodo == "POST") {
+        if (empty($_GET['login'])) {
+            echo json_encode([
+                'error' => true,
+                'message' => "Parâmetro 'login' está ausente!"
+            ]);
+            exit;
+        }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+            exit;
+        }
+
         require_once(realpath(dirname(__FILE__) . '/POST.php'));
-        // if (Usuarios::verificar('supervisores')) {
-        // } else {
-        //     echo json_encode([
-        //         'error' => true,
-        //         'message' => 'Você não está logado, ou seu token é inválido.'
-        //     ]);
-        //     exit;
-        // }
     }
 
     if ($metodo == 'DELETE') {
+        if (empty($_GET['login'])) {
+            echo json_encode([
+                'error' => true,
+                'message' => "Parâmetro 'login' está ausente!"
+            ]);
+            exit;
+        }
+
+        $login = $_GET['login'];
+        $funcao = 'supervisores';
+
+        if (!Usuarios::validarToken($login)) {
+            echo json_encode([
+                'error' => true,
+                'message' => 'Você não está logado, ou seu token é inválido.'
+            ]);
+            exit;
+        }
+
+        if (!Usuarios::autorizar($funcao, $login)) {
+            echo json_encode([
+                'error' => true,
+                "message" => "Acesso não autorizado!"
+            ]);
+            exit;
+        }
+
         require_once(realpath(dirname(__FILE__) . '/DELETE.php'));
-        // if (Usuarios::verificar('supervisores')) {
-        // } else {
-        //     echo json_encode([
-        //         'error' => true,
-        //         'message' => 'Você não está logado, ou seu token é inválido.'
-        //     ]);
-        //     exit;
-        // }
     }
 }
