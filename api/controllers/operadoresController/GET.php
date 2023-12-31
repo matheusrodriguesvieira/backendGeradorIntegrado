4948ab58d0d5a8e51d486ea1ab8382b113193a9b
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2023-12-30 21:18:50 -0300

    ajustes dos filtros dos operadores

--- api/controllers/operadoresController/GET.php
+++ api/controllers/operadoresController/GET.php
@@ -22,13 +22,36 @@ if ($acao == 'index' && $parametro == '') {
 
         echo json_encode($obj);
         exit;
-    } else {
+    }
+
+    if (empty($_GET['gerencia'])) {
 
         $turma = $_GET['turma'];
 
         $db = DB::connect();
         $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, usuarios.turma, gerencia.nome as gerencia, usuarios.matriculasupervisor, operadores.disponivel from usuarios, operadores, gerencia where operadores.matricula = usuarios.matricula and gerencia.id = usuarios.idgerencia and usuarios.matricula > 5 and usuarios.turma = ?");
         $sql->execute([$turma]);
+        $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!$obj) {
+            $response = array(
+                "message" => "Nenhum operador encontrado!"
+            );
+            echo json_encode($response);
+            exit;
+        }
+
+        echo json_encode($obj);
+        exit;
+    }
+
+    if (empty($_GET['turma'])) {
+
+        $gerencia = $_GET['gerencia'];
+
+        $db = DB::connect();
+        $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, usuarios.turma, gerencia.nome as gerencia, usuarios.matriculasupervisor, operadores.disponivel from usuarios, operadores, gerencia where operadores.matricula = usuarios.matricula and gerencia.id = usuarios.idgerencia and usuarios.matricula > 5 and usuarios.idgerencia = ?");
+        $sql->execute([$gerencia]);
         $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
 
         if (!$obj) {
