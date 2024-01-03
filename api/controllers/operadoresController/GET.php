f092c5a8dd7c08d08e1ba8672d7eaf6c2d6e8d60
matheusrodriguesvieira <matheusrodriguespgm@gmail.com>
2024-01-03 01:40:41 -0300

    ajuste na rota index, operadores

--- api/controllers/operadoresController/GET.php
+++ api/controllers/operadoresController/GET.php
@@ -5,6 +5,28 @@ if ($acao == 'index' && $parametro == '') {
     // PEGA TODOS OS OPERADORES
     // ---------------------------------------
 
+    if (!empty($_GET['turma']) || !empty($_GET['gerencia'])) {
+
+        $turma = $_GET['turma'];
+        $gerencia = $_GET['gerencia'];
+
+        $db = DB::connect();
+        // $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, operadores.disponivel, usuarios.matriculasupervisor from operadores, usuarios");
+        $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, usuarios.turma, gerencia.nome as gerencia, usuarios.matriculasupervisor, operadores.disponivel from usuarios, operadores, gerencia where operadores.matricula = usuarios.matricula and gerencia.id = usuarios.idgerencia and usuarios.matricula > 5 and usuarios.turma = ? and usuarios.idgerencia = ?");
+        $sql->execute([$turma, $gerencia]);
+        $obj = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        if (!$obj) {
+            $response = array(
+                "message" => "Nenhum operador encontrado!"
+            );
+            echo json_encode($response);
+            exit;
+        }
+
+        echo json_encode($obj);
+        exit;
+    }
     if (empty($_GET['turma']) || empty($_GET['gerencia'])) {
         $db = DB::connect();
         // $sql = $db->prepare("SELECT operadores.matricula, usuarios.nome, operadores.disponivel, usuarios.matriculasupervisor from operadores, usuarios");
